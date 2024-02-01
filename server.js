965edfbf1d010a58e1d67277a113d9c428c0870a
Madhurima <madhurimavispute61@gmail.com>
2024-02-01 16:07:46 +0530

    css pages added

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/contact', function (req, res){
- console.log(req.body)
+var x = req.body.fname
+var s = req.body.lname
+var p = req.body.email
+var q = req.body.message
+var y = "Thank you, "+ x + s + p + q +". we will contact you soon"
+ console.log(x, s, p, q, y)
+ res.render("contact.ejs", {"text":y })
 
 });
 
@@ -30,13 +36,14 @@ app.get('/services', function (req, res) {
         res.render("services.ejs", {})
 
 });
+
 app.get('/contact', function (req, res) {
         console.log(__dirname)
-        res.render("home.ejs", {})
+        res.render("contact.ejs", {"text": 'Fill this Form to get Connected With Us'})
 
 });
 
 
 app.listen(8000, function (res, req) {
-        console.log("server start")
+        console.log("port is running 8000")
 })
